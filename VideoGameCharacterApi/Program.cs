d59764c4af9a4214688a592e125fc3e1f3203dca
gavr7k <gavrilovich2002@gmail.com>
2025-12-28 17:24:06 +0300

    Добавил EF Core , добавил контекст, подключение

--- VideoGameCharacterApi/Program.cs
+++ VideoGameCharacterApi/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using VideoGameCharacterApi.Data;
 using VideoGameCharacterApi.Services;
 
 
@@ -11,6 +13,9 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IVideoGameCharacterService, VideoGameCharacterService>();
 
