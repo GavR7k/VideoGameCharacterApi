4a17631e8e7563e188c419c317ea9f41d6465907
gavr7k <gavrilovich2002@gmail.com>
2025-12-28 19:17:20 +0300

    Миграция + внедрение в сервис

--- VideoGameCharacterApi/Services/VideoGameCharacterService.cs
+++ VideoGameCharacterApi/Services/VideoGameCharacterService.cs
@@ -1,16 +1,16 @@
-﻿using VideoGameCharacterApi.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameCharacterApi.Data;
+using VideoGameCharacterApi.Models;
 
 namespace VideoGameCharacterApi.Services
 {
     public class VideoGameCharacterService : IVideoGameCharacterService
     {
-        static List<Character> characters = new List<Character> {
-
-            new Models.Character {Id = 1, Name = "Mario", Role ="Hero", Game = "Super Mario"},
-            new Models.Character {Id = 2, Name = "Alice", Role ="Main Hero", Game = "Resident Evil"},
-            new Models.Character {Id= 3, Name = "Price", Role="Captain", Game = " Call Of Duty"},
-
-        };
+        private readonly AppDbContext _db;
+        public VideoGameCharacterService(AppDbContext db)
+        {
+            _db = db;
+        }
 
         public Task<Character> AddAsync(Character character)
         {
@@ -24,13 +24,13 @@ namespace VideoGameCharacterApi.Services
 
         public async Task<List<Character>> GetAllAsync()
         {
-            return await Task.FromResult(characters);
+            return await _db.Characters.ToListAsync();
         }
 
         public async Task<Character?> GetByIdAsync(int id)
         {
-            var character = characters.FirstOrDefault(c => c.Id == id);
-            return await Task.FromResult(character);
+            var character = await _db.Characters.FirstOrDefaultAsync(c=>c.Id == id);
+            return character;
         }
 
         public Task<bool> UpdateAsync(int id, Character character)
